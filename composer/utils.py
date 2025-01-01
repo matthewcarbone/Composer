55b2771b7991408812da0fd1b72e91d32dccb590
Matthew Carbone <x94carbone@gmail.com>
2025-01-01 16:40:41 -0500

    Add get_file_hash function to utils

--- composer/utils.py
+++ composer/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 from time import perf_counter
 
 
@@ -12,3 +13,14 @@ class Timer:
     @property
     def dt(self):
         return self._time
+
+
+def get_file_hash(file_path):
+    """Compute and return the SHA-256 hash of the given file."""
+
+    hasher = hashlib.sha256()
+    block_size = 8192  # read in chunks of 8 KB
+    with open(file_path, "rb") as f:
+        while chunk := f.read(block_size):
+            hasher.update(chunk)
+    return hasher.hexdigest()
